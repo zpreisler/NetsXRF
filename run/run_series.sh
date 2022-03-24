5dc18d2ace45842928c12ffc60405d99a94a20a4
Zdenek Preisler <z.preisler@gmail.com>
2022-03-24 15:53:36 +0000

    run

--- run/run_series.sh
+++ run/run_series.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
 for i in `seq 5`
 do
-	../train.py config_cuda.yaml -n ResNet1_$i
+	for j in `seq 3`
+	do
+		nice -19 ../../train.py config_cuda.yaml -n ResNet1_$i\_$j &
+	done
+	wait
 done
