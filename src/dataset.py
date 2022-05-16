7bc1ea5c4dcfa9c0a94f86ff222e8e1d73d0eec8
Andrea <busaccandrea@gmail.com>
2022-05-16 17:05:02 +0200

    changed default scale factors, removed useless print

--- src/dataset.py
+++ src/dataset.py
@@ -21,7 +21,7 @@ class SynthSpectraDataset(Dataset):
     
     def __init__(self,data,labels,scales=[None]):
         super().__init__()
-        if scales ==[None]:self.scales = [0.1,0.2,0.3,0.4,0.5,0.7,1.0,1.1]
+        if scales ==[None]:self.scales = [0.1,0.2,0.3,0.4,0.5,0.6,0.65,0.7,0.75,0.8,0.9,1.0,1.1]
         else:self.scales = scales
 
         self.data = data
@@ -30,7 +30,6 @@ class SynthSpectraDataset(Dataset):
     def __getitem__(self,idx):
         scale_factor = np.random.choice(self.scales)
         spec = self.data[idx]*scale_factor
-        print(spec,scale_factor, idx)
         return torch.poisson(spec), self.labels[idx]*scale_factor
     
     def __len__(self):
